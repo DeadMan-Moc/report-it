a45e73af84548f42778637ecd37e952bf61d5ac2
exabanis <xabanisat2webmail.co.za>
2019-09-11 17:55:50 +0200

    index js

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var bodyParser = require('body-parser');
 var app = express();
 var session = require('express-session');
 var engine = require('ejs-locals');
-var nodemailer = require('nodemailer');
+const port = 3000;
 
-var PORT = 3000;
-app.listen(PORT || 3000, function () {
-  console.log('Connected !');
-});
+// var PORT = 3000;
+// app.listen(PORT || 3000, function () {
+//   console.log('Connected !');
+// });
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`report it server listening on port ${port}!`))
